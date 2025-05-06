14cdccdf2eaea17f0b7b40e16dcf3c4c18909004
Nivedithaanbu <154871700+Nivedithaanbu@users.noreply.github.com>
2025-05-06 10:34:07 +0000

    Add Streamlit pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Multipage App" ,
+    page_icon="🤓"
 )
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
